d45a3d1844c71abb1c2f8c2cdc47ca26471474b8
João Gabriel <joao@zapiet.com>
2022-08-19 19:00:44 -0300

    feat: TableHead component

---
+++ components/TableHead.vue
@@ -0,0 +1,15 @@
+<template>
+  <td
+    :class="isFirst ? 'py-3.5 pl-4 pr-3 sm:pl-6' : 'px-3 py-3.5'"
+    class="text-left text-sm font-semibold text-gray-900"
+  >
+    {{ title }}
+  </td>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'TableHead',
+  props: ['title', 'isFirst'],
+}
+</script>
