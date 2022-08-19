e38bb6837edadcfc916aaee06e4b1a07b180f1c5
João Gabriel <joao@zapiet.com>
2022-08-19 18:40:02 -0300

    feat: table component

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,9 @@
     <AppHeader />
     <div class="bg-gray-100 h-screen min-h-screen w-screen min-w-screen">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <Nuxt />
+        <div class="py-12">
+          <Nuxt />
+        </div>
       </div>
     </div>
   </div>
