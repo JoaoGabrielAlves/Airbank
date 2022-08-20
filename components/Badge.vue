c4b37c22ef7ce25e0cbaaaf1126a09b1d5c2bf85
João Gabriel <joao@zapiet.com>
2022-08-20 18:41:52 -0300

    refactor: using slot on badge component

--- components/Badge.vue
+++ components/Badge.vue
@@ -1,9 +1,9 @@
 <template>
   <span
-    class="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium text-black"
+    class="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium text-gray-900"
     v-bind:style="{ 'background-color': '#' + color }"
   >
-    {{ color }}
+    <slot></slot>
   </span>
 </template>
 
